74090b56a9a60f5037127893b3730a85418e4b25
sweetfood-dev <kjsii123@hanmail.net>
2021-01-22 09:06:56 +0900

    category, years Navigation add

---
+++ _posts/Algorithm/DP/2021-01-22-Algorithm-DP-이론.md
@@ -0,0 +1,8 @@
+---
+title: 공부해서 넣자
+categories:
+- Algorithm
+- DP
+---
+
+알겠징?
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "공부해서 넣자",
+        "excerpt":"알겠징?  ","categories": ["Algorithm","DP"],
+        "tags": [],
+        "url": "https://sweetfood-dev.github.io/algorithm/dp/Algorithm-DP-%EC%9D%B4%EB%A1%A0/",
+        "teaser": null
+      },{
+        "title": "arc 공부해서 수정",
+        "excerpt":"ㅇㅇ  ","categories": ["iOS"],
+        "tags": [],
+        "url": "https://sweetfood-dev.github.io/ios/iOS-ARC-%EC%A0%95%EB%A6%AC/",
+        "teaser": null
+      }]
